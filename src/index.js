d4a1492fa59b95728f8be484df6f9f74a8a2580f
Gernot Pointner <gernot.pointner@googlemail.com>
2017-09-16 21:02:07 +0200

    Add dummy quotes view

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-// nothing yet
+var m = require("mithril");
+var quotes_view = require("./views/quote_view.js")
+var applicationRoot = document.getElementById("appContainer");
+
+m.route(
+    applicationRoot, "/quotes",
+    {"/quotes" : quotes_view}
+);
