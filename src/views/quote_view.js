fd7cab8535caed4efdf3f319792ec9066d52863c
Gernot Pointner <gernot.pointner@googlemail.com>
2017-09-16 21:48:45 +0200

    Add dummy quote entries

--- src/views/quote_view.js
+++ src/views/quote_view.js
@@ -2,6 +2,18 @@ var m = require("mithril");
 
 module.exports = {
     view: function(){
-        return m("div", "The list of quotes");
+        return m("table",{"class" : "table"}, [
+            m("caption", "Some dev quotes"),
+            m("thead", [
+                m("th", "quote"),
+                m("th", "author")
+            ]),
+            m("tbody", [
+                m("tr", [
+                    m("td", "Quote"),
+                    m("td", "Author")
+                ])
+            ])
+        ]);
     }
 };
