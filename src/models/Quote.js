9c14a0f593f39317840a706539390277788ddffa
Gernot Pointner <gernot.pointner@googlemail.com>
2017-09-17 08:03:28 +0200

    Add QuoteModel

---
+++ src/models/Quote.js
@@ -0,0 +1,6 @@
+module.exports = {
+    list : [
+        {"quote" : "Premature optimization is the root of all evil", "author" : "Donald Knuth"},
+        {"quote" : "If you try to solve a problem with regex, you will have two problems", "author" : "Unknown"}
+    ]
+};
--- src/views/QuoteList.js
+++ src/views/QuoteList.js
@@ -1,19 +1,24 @@
 var m = require("mithril");
+var quoteModel = require("../models/Quote.js");
 
 module.exports = {
     view: function(){
+
+        // render quote list items
+        var quoteListItems = quoteModel.list.map(function(quoteItem){
+            return m("tr", [
+                m("td", quoteItem.quote),
+                m("td", quoteItem.author)
+            ]);
+        });
+
         return m("table",{"class" : "table"}, [
             m("caption", "Some dev quotes"),
             m("thead", [
                 m("th", "quote"),
                 m("th", "author")
             ]),
-            m("tbody", [
-                m("tr", [
-                    m("td", "Quote"),
-                    m("td", "Author")
-                ])
-            ])
+            m("tbody", quoteListItems)
         ]);
     }
 };
